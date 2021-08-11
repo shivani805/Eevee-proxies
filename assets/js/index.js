ded4ffb0e25daabd5c514908bb8269f79e5d6815
shivani805 <shivanipanda30@gmail.com>
2021-08-11 13:27:42 +0530

    correction in design

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,13 +2,16 @@ function myFunction1() {
   var dots = document.getElementById("dots1");
   var moreText = document.getElementById("more1");
   var btnText = document.getElementById("myBtn1");
+  var btn = document.getElementById("cancelBtn1");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
     btnText.style.display = "inline";
     moreText.style.display = "none";
+    btn.style.display = "none";
   } else {
     dots.style.display = "none";
+    btn.style.display = "inline";
     btnText.style.display = "none";
     moreText.style.display = "inline";
   }
@@ -18,13 +21,16 @@ function myFunction2() {
   var dots = document.getElementById("dots2");
   var moreText = document.getElementById("more2");
   var btnText = document.getElementById("myBtn2");
+  var btn = document.getElementById("cancelBtn2");
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
     btnText.style.display = "inline";
     moreText.style.display = "none";
+    btn.style.display = "none";
   } else {
     dots.style.display = "none";
+    btn.style.display = "inline";
     btnText.style.display = "none";
     moreText.style.display = "inline";
   }
