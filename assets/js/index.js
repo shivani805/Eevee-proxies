ca2164b39a7198834991e46cfaa3e3100031afa0
shivani805 <shivanipanda30@gmail.com>
2021-08-12 13:37:15 -0700

    code refactor

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,9 @@
-function myFunction1() {
-  var dots = document.getElementById("dots1");
-  var moreText = document.getElementById("more1");
-  var btnText = document.getElementById("myBtn1");
-  var btn = document.getElementById("cancelBtn1");
 
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.style.display = "inline";
-    moreText.style.display = "none";
-    btn.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btn.style.display = "inline";
-    btnText.style.display = "none";
-    moreText.style.display = "inline";
-  }
-}
-
-function myFunction2() {
-  var dots = document.getElementById("dots2");
-  var moreText = document.getElementById("more2");
-  var btnText = document.getElementById("myBtn2");
-  var btn = document.getElementById("cancelBtn2");
+function myFunction(dots,more,myBtn,cancelBtn) {
+  var dots = document.getElementById(dots);
+  var moreText = document.getElementById(more);
+  var btnText = document.getElementById(myBtn);
+  var btn = document.getElementById(cancelBtn);
 
   if (dots.style.display === "none") {
     dots.style.display = "inline";
